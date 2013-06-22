165df521a38e69cc02a0ce70ff7febb77e786160
mvoidex <voidex@live.com>
2013-06-22 16:13:10 +0400

    Refactored 'hsinspect'

--- tools/hsinspect.hs
+++ tools/hsinspect.hs
@@ -4,13 +4,12 @@ module Main (
 	main
 	) where
 
-import Control.Exception (handle, SomeException, throwIO)
-import Control.Monad
-import Control.Monad.Error (runErrorT)
+import Control.Exception (handle, SomeException)
+import Control.Monad ((>=>))
+import Control.Monad.Error (ErrorT, runErrorT)
 import Control.Monad.IO.Class (liftIO)
 import Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as L (unpack)
-import Data.String (fromString)
 import System.Environment (getArgs)
 import System.IO
 import System.Console.GetOpt
@@ -27,27 +26,18 @@ import HsDev.Project.JSON (encodeProject)
 commands :: [Command (IO ())]
 commands = addhelp "hsinspect" id printUsage [
 	cmd ["module"] [] "inspect installed module" [
-		Option ['g'] ["ghc"] (ReqArg return "opt") "option to pass to ghc in ghc-mod and hdocs"] $ \ghc_opts as -> case as of
-			[] -> putStrLn "Specify at least one module"
-			modules -> do
-				rs <- forM modules $ \m -> do
-					r <- runErrorT (browse m >>= liftIO . loadDocs ghc_opts)
-					return (fromString m .= either errorStr encodeModule r)
-				putJSON $ object rs,
-	cmd_ ["file"] [] "inspect file" $ \as -> case as of
-		[] -> putStrLn "Specify at least one file"
-		files -> do
-			rs <- forM files $ \f -> do
-				r <- runErrorT $ inspectFile f
-				return (fromString f .= either errorStr encodeModule r)
-			putJSON $ object rs,
-	cmd_ ["cabal"] [] "inspect cabal" $ \as -> case as of
-		[] -> putStrLn "Specify at least one file"
-		cabals -> do
-			cs <- forM cabals $ \c -> do
-				r <- runErrorT $ readProject c
-				return (fromString c .= either errorStr encodeProject r)
-			putJSON $ object cs]
+		Option ['g'] ["ghc"] (ReqArg return "opt") "option to pass to ghc in ghc-mod and hdocs"] $
+			\ghc_opts as -> oneArg as encodeModule (browse >=> liftIO . loadDocs ghc_opts),
+	cmd_ ["file"] [] "inspect file" $ \as -> oneArg as encodeModule inspectFile,
+	cmd_ ["cabal"] [] "inspect cabal" $ \as -> oneArg as encodeProject readProject]
+
+	where
+		oneArg :: [String] -> (a -> Value) -> (String -> ErrorT String IO a) -> IO ()
+		oneArg [] _ _ = putJSON $ errorStr "Not enough arguments"
+		oneArg [s] toj onArg = do
+			r <- runErrorT $ onArg s
+			putJSON $ either errorStr toj r
+		oneArg _ _ _ = putJSON $ errorStr "Too much arguments"
 
 main :: IO ()
 main = handle handleError $ do
@@ -67,9 +57,7 @@ main = handle handleError $ do
 			printUsage
 
 		handleError :: SomeException -> IO ()
-		handleError e = do
-			putJSON . errorStr . show $ e
-			throwIO e
+		handleError = putJSON . errorStr . show
 
 putJSON :: Value -> IO ()
 putJSON = putStrLn . L.unpack . encode
