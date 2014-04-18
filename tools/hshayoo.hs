d4c19137fa08239233d3cdacde3ee733522bb9e5
mvoidex <voidex@live.com>
2014-04-18 18:23:32 +0400

    hshayoo tool

---
+++ tools/hshayoo.hs
@@ -0,0 +1,15 @@
+module Main (
+	main
+	) where
+
+import Control.Monad (liftM)
+import HsDev.Tools.Hayoo
+
+import Tool
+
+main :: IO ()
+main = toolMain "hshayoo" [
+	jsonCmd_ [] ["query"] "search in hayoo" hayoo']
+	where
+		hayoo' [q] = liftM (map hayooAsDeclaration . hayooFunctions) $ hayoo q
+		hayoo' _ = toolError "Invalid arguments"
