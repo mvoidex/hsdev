4dd1f390afa799f19db0fc9dcdeca474bfb6012c
mvoidex <voidex@live.com>
2014-05-27 07:49:21 +0400

    Added Display module

---
+++ src/HsDev/Display.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+
+module HsDev.Display (
+	Display(..)
+	) where
+
+import Data.Maybe (fromMaybe)
+
+import HsDev.Cabal
+import HsDev.Symbols.Location
+import HsDev.Project
+
+class Display a where
+	display :: a -> String
+	displayType :: a -> String
+
+instance Display Cabal where
+	display Cabal = "cabal"
+	display (Sandbox p) = "sandbox " ++ p
+	displayType _ = "cabal"
+
+instance Display ModuleLocation where
+	display (FileModule f _) = f
+	display (CabalModule _ _ n) = n
+	display (OtherModuleSource s) = fromMaybe "" s
+	displayType _ = "module"
+
+instance Display Project where
+	display = projectName
+	displayType _ = "project"
+
+instance Display FilePath where
+	display = id
+	displayType _ = "path"
