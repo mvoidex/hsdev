015c69263c6e29a321f7acab42559a4ff803b023
mvoidex <voidex@live.com>
2014-09-13 06:13:50 +0400

    Added 'trim' function

--- src/HsDev/Tools/Ghc/Prelude.hs
+++ src/HsDev/Tools/Ghc/Prelude.hs
@@ -1,9 +1,15 @@
 module HsDev.Tools.Ghc.Prelude (
-	reduce, one
+	reduce, one, trim
 	) where
+
+import Data.Char (isSpace)
 
 reduce :: ([a] -> a) -> [a] -> [a]
 reduce = (return .)
 
 one :: a -> [a]
 one = return
+
+trim :: String -> String
+trim = p . p where
+	p = reverse . dropWhile isSpace
